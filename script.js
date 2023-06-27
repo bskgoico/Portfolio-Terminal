78c51e9be3229ef7572561007fc2646ea4aedee9
fnkbsk <igoico10@gmail.com>
2023-06-27 19:12:25 -0300

    first-commit

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const promtInput = document.getElementById('promtInput');
 const terminal = document.getElementById('terminal');
 const terminalWindow = document.getElementById('terminalWindow');
 const date = document.getElementById('date');
+
+promtInput.focus();
+date.innerText = new Date().toDateString()
+terminalWindow.addEventListener('click', () => promtInput.focus());
