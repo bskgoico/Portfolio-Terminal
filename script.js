46f8615853aa02a652c348fab56318ea02ec62a0
fnkbsk <igoico10@gmail.com>
2023-06-27 19:32:10 -0300

    first-commit

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const date = document.getElementById('date');
 promtInput.focus();
 date.innerText = new Date().toDateString()
 terminalWindow.addEventListener('click', () => promtInput.focus());
+
+promtInput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        enterCommand(event);
+    }
+})
+
+const enterCommand = (event) => {
+    const promtElement = document.getElementById('promptClone').cloneNode(true);
+    promtElement.classList.remove('hidden');
+    promtElement.getElementByClassName('promtCloneInput')[0].innerHTML = event.target.value;
+    promtElement.setAttribute('id', null);
+    promtElement.getElementByClassName('promtCloneContent')[0].appendChild(selectCommandBlock(event.target.valueo));
+}
