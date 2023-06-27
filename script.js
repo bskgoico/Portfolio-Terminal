4e2861a77f1cfdf914000b4977bfef1ce0180916
fnkbsk <igoico10@gmail.com>
2023-06-27 18:28:29 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const promtInput = document.getElementById('promtInput');
+const terminal = document.getElementById('terminal');
+const terminalWindow = document.getElementById('terminalWindow');
+const date = document.getElementById('date');
